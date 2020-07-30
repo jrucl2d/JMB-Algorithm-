06bb7c66a2e3e20738c3d6af64d85643d080ef3b
jrucl2d <ysk789465@gmail.com>
2020-07-30 20:24:29 +0900

    QUADTREE solved

--- JMB_P189_QUADTREE.cpp
+++ JMB_P189_QUADTREE.cpp
@@ -1,60 +1,57 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
-// 4
-// w
-// xbwwb
-// xbwxwbbwb
-// xxwwwbxwxwbbbwwxxxwwbbbwwwbb
 
-void reverse(string s)
+string reverse(string s)
 {
-    // if (s.size() == 1) // b이거나 w이면
-    // {
-    //     return;
-    // }
-    int cp[4] = {1, 0, 0, 0};
-    int cpi = 0;
-    int cpn = 0;
-    bool checkStart = false;
-    // xxwwwbxwxwbbbwwxxxwwbbbwwwwbb
-    for (int i = 1; i < s.size(); i++)
+    if (s.size() == 1) // b이거나 w이면 stop
+        return s;
+    // find points
+    vector<string> subst(4, "");
+    int cp[4] = {1, 0, 0, 0}; // slice의 시작점을 저장하는 배열
+    int cpI = 0;
+    int cnt = 0; // 해당 slice의 길이 후보
+    if (s[1] == 'x')
+        cnt = 5;
+    else
+        cnt = 1;
+    subst[cpI] += s[1];
+    for (int i = 2; i < s.size(); i++)
     {
-        if (i == cp[cpi] + cpn && i != 1 && checkStart)
+        if (i == cp[cpI] + cnt) // 다음 slice의 시작점
         {
-            cp[++cpi] = i;
-            checkStart = false;
+            cp[++cpI] = i;
+            cnt = 1;
         }
-        if (s[i] == 'x' && checkStart && i < cp[cpi] + cpn)
+        if (s[i] == 'x') // x를 만나면 slice 길이 후보를 4를 더함
         {
-            cpn += 4;
+            cnt += 4;
         }
-        else if (s[i] == 'x' && !checkStart)
-        {
-            checkStart = true;
-            cpn = 5;
-        }
-        else if (s[i] != 'x' && !checkStart && i != 1)
-        {
-            cp[++cpi] = i;
-            cpn = 1;
-        }
+        subst[cpI] += s[i];
     }
-    for (int i = 0; i < 4; i++)
-    {
-        cout << cp[i] << endl;
-    }
+    string res = "x";
+    res += reverse(subst[2]);
+    res += reverse(subst[3]);
+    res += reverse(subst[0]);
+    res += reverse(subst[1]);
+
+    return res;
 }
 
 int main()
 {
     int c = 0;
-    reverse("xbwxwbbwb");
-    // cin >> c;
-    // string tmp = "";
-    // while (c-- > 0)
-    // {
-    //     cin >> tmp;
-    //     reverse(tmp);
-    // }
+    cin >> c;
+    string tmp = "";
+    while (c-- > 0)
+    {
+        cin >> tmp;
+        if (tmp.size() == 1) // 예외 사례
+        {
+            cout << tmp << endl;
+            continue;
+        }
+        cout << reverse(tmp) << endl;
+    }
 }
